63c75f78b2344f800ec87002844af5ed9de0c070
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-08-04 13:49:44 -0700

    leo button

--- js/init.js
+++ js/init.js
@@ -21,3 +21,13 @@ fetch("https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=taurus&day=today", {
 .catch(err => {
 	console.error(err);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
+        
